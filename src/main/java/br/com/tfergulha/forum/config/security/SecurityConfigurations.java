104f31fd28d575b37210adc1c691b72cb5bbf0dc
Túlio Fergulha <tfergulha@gmail.com>
2021-11-16 23:08:17 -0300

    Adicionado swagger

--- src/main/java/br/com/tfergulha/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/tfergulha/forum/config/security/SecurityConfigurations.java
@@ -63,5 +63,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     // Configurações de recursos estáticos (arquivos css, js, imagens)
     @Override
     public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**",
+            "/configuration/**", "/swagger-resources/**");
     }
 }
